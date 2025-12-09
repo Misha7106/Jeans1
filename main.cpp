87c44dfa3956d42abac7b4bda3711690e937dcf6
alex_kpl <swezkyviktor@gmail.com>
2025-12-10 00:18:33 +0500

    Sasha added something in func2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+vector<string> createTestData() {
+    vector<string> data;
+    data.push_back("Строка 1 - тестовые данные");
+    data.push_back("Строка 2 - для отладки функций");
+    data.push_back("Строка 3 - вывод на экран");
+    data.push_back("Строка 4 - запись в файл");
+    data.push_back("Строка 5 - конец теста");
+    return data;
+}
 
 // Функция № 1: Чтение строк из файла в вектор
 vector<string> readLinesFromFile(const string& filename) {
@@ -14,17 +25,39 @@ vector<string> readLinesFromFile(const string& filename) {
 
 // Функция № 2: Вывод строк на экран
 void printLines(const vector<string>& lines) {
-    // Здесь будет код для вывода строк на экран
-    // TODO: Реализовать вывод на экран
+    if (lines.empty()) {
+        cout << "Нет данных для вывода" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < lines.size(); i++) {
+        cout << lines[i] << endl;
+    }
 }
 
 // Функция № 3: Запись строк в файл
 void writeLinesToFile(const vector<string>& lines, const string& filename) {
-    // Здесь будет код для записи строк в файл
-    // TODO: Реализовать запись в файл
+    if (lines.empty()) {
+        cerr << "Ошибка: вектор пуст" << endl;
+        return;
+    }
+
+    ofstream file(filename);
+    if (!file) {
+        cerr << "Ошибка открытия файла: " << filename << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < lines.size(); i++) {
+        file << lines[i] << endl;
+    }
+
+    file.close();
 }
 
 int main() {
+
+    setlocale(LC_ALL, "russian");
     // Последовательный вызов трех функций
     string inputFilename = "input.txt";
     string outputFilename = "output.txt";
